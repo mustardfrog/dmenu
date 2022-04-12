076d624aad087b0ff0e0c9b8b71b1c01d42dbf34
Frog <frogdoctor@tutanota.com>
2022-04-12 14:27:59 +0630

    changed colorscheme

--- config.def.h
+++ config.def.h
@@ -5,17 +5,17 @@ static int topbar = 0;                      /* -b  option; if 0, dmenu appears a
 static int fuzzy = 1;                      /* -F  option; if 0, dmenu doesn't use fuzzy matching     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"monospace:size=10"
+	"ubuntu mono:size=13"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel] = { "#eeeeee", "#005577" },
-	[SchemeOut] = { "#000000", "#00ffff" },
+    [SchemeNorm] = { "#bbbbbb", "#222222" },
+    [SchemeSel] = { "#eeeeee", "#000000" },
+    [SchemeOut] = { "#000000", "#eeeeee" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
-static unsigned int lines      = 6;
+static unsigned int lines      = 4;
 
 /*
  * Characters not considered part of a word while deleting words
