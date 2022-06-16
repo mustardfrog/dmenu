f05661aec7da83c4658c9ab6186abd32d5c54c7d
Mr. Frog <frogdoctor@tutanota.com>
2022-06-16 12:48:35 +0630

    changed color to pink

--- config.h
+++ config.h
@@ -5,13 +5,13 @@ static int topbar = 0;                      /* -b  option; if 0, dmenu appears a
 static int fuzzy = 1;                      /* -F  option; if 0, dmenu doesn't use fuzzy matching     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"ubuntu mono:size=13"
+	"Jetbrains Mono:size=13"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	        /*     fg         bg       */
-    [SchemeNorm] = { "#89e8dd", "#000000" },
-    [SchemeSel] = { "#eeeeee", "#2a3158" },
+    [SchemeNorm] = { "#000000", "#eeeeee" },
+    [SchemeSel] = { "#000000", "#ff748c" },
     [SchemeOut] = { "#242940", "#89d8cc" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
